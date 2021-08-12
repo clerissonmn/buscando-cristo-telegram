6661bb22b7333c8a4b5833f0f76b89d8c32c9f48
clerisson nascimento <clerisson.mn@gmail.com>
2021-08-12 14:47:14 -0300

    agora tá tudo ok

--- botbuscando_cristo.py
+++ botbuscando_cristo.py
@@ -34,40 +34,37 @@ with client:
     client.loop.run_until_complete(apaga_msg_todas(client=client, canal=canal))
     # client.loop.run_until_complete(teste(client=client, canal=canal))
 
-def envia_mensagens_para_telegram(dia=None):
-    hoje = _dias_da_semana[dt.today().strftime("%A")]
-    programacao_lista = ["Missa", "Confissão"]
-    cidade_lista = ["Belém", "Ananindeua"]
-    
-    if not dia:
-        dia_da_semana = hoje #['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
 
-    for programacao in programacao_lista:
-        for cidade in cidade_lista:
-            print(f"==> Enviando {programacao} em {cidade} para {dia_da_semana}")
+hoje = _dias_da_semana[dt.today().strftime("%A")]
+programacao_lista = ["Missa", "Confissão"]
+cidade_lista = ["Belém", "Ananindeua"]
 
-            resultado = pega_horarios(
-                df=df,
-                programacao=programacao,
-                natureza="Presencial",
-                cidade=cidade,
-                bairro="todos",
-                dia_da_semana=dia_da_semana,
-                formato_saida="dict",
+dia_da_semana = hoje #['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+
+for programacao in programacao_lista:
+    for cidade in cidade_lista:
+        print(f"==> Enviando {programacao} em {cidade} para {dia_da_semana}")
+
+        resultado = pega_horarios(
+            df=df,
+            programacao=programacao,
+            natureza="Presencial",
+            cidade=cidade,
+            bairro="todos",
+            dia_da_semana=dia_da_semana,
+            formato_saida="dict",
+        )
+
+        # Formata em uma mensagem
+        mensagem = formata_mensagem(
+            resultado=resultado,
+            cidade=cidade,
+            programacao=programacao,
+            dia=dia_da_semana,
+        )
+
+        # Envia a mensagem
+        with client:
+            client.loop.run_until_complete(
+                envia_mensagem(client=client, canal=canal, mensagem=mensagem)
             )
-
-            # Formata em uma mensagem
-            mensagem = formata_mensagem(
-                resultado=resultado,
-                cidade=cidade,
-                programacao=programacao,
-                dia=dia_da_semana,
-            )
-
-            # Envia a mensagem
-            with client:
-                client.loop.run_until_complete(
-                    envia_mensagem(client=client, canal=canal, mensagem=mensagem)
-                )
-
-#%% [ Automação ]
